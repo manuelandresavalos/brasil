115fd3f4f073bb916260585e5f95f64182e5fc5f
manuelandresavalos <manuelandresavalos@gmail.com>
2017-02-24 20:08:01 -0300

    Login integration with AdminLTE

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -33,6 +33,65 @@ class Admin extends CI_Controller {
 
 	public function index()
 	{
-		$this->load->view('Admin');
+		// Check if user is logged_in
+		if (isset($this->session->userdata['logged_in'])) {
+			$username = ($this->session->userdata['logged_in']['username']);
+			$email = ($this->session->userdata['logged_in']['email']);
+			$this->load->view('Admin');
+		} else {
+			$this->load->view('Login');
+		}
 	}
+
+	// Check for user login process
+    public function user_login_process()
+    {
+        $this->form_validation->set_rules('username', 'Username', 'trim|required|xss_clean');
+        $this->form_validation->set_rules('password', 'Password', 'trim|required|xss_clean');
+        if ($this->form_validation->run() == FALSE) {
+            if (isset($this->session->userdata['logged_in'])) {
+                //$this->load->view('Admin');
+                redirect('Admin', 'refresh');
+            } else {
+                $this->load->view('Login');
+            }
+        } else {
+            $data   = array(
+                'username' => $this->input->post('username'),
+                'password' => $this->input->post('password')
+            );
+            $result = $this->login_database->login($data);
+            if ($result == TRUE) {
+
+                $username = $this->input->post('username');
+                $result   = $this->login_database->read_user_information($username);
+                if ($result != false) {
+                    $session_data = array(
+                        'username' => $result[0]->user_name,
+                        'email' => $result[0]->user_email
+                    );
+                    // Add user data in session
+                    $this->session->set_userdata('logged_in', $session_data);
+                    redirect('Admin', 'refresh');
+                }
+            } else {
+                $data = array(
+                    'error_message' => 'Invalid Username or Password'
+                );
+                $this->load->view('Login', $data);
+            }
+        }
+    }
+
+    // Logout from admin page
+    public function logout()
+    {
+        // Removing session data
+        $sess_array = array(
+            'username' => ''
+        );
+        $this->session->unset_userdata('logged_in', $sess_array);
+        $data['message_display'] = 'Successfully Logout';
+        $this->load->view('login', $data);
+    }
 }
